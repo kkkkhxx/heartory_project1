1c4257fc82c22cb61a91ee59637ea4c018c16415
kkkkhxx <tanyaratclub@gmail.com>
2025-10-04 11:23:15 +0700

    test demo

---
+++ src/test/java/com/zetcode/AppTest.java
@@ -0,0 +1,11 @@
+package com.zetcode;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testExample() {
+        assertEquals(2, 1 + 1);
+    }
+}
